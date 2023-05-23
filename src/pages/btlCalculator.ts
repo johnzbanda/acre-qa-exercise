3409b6defc80f5dbf66b7572fdb1dcf49dd836d6
Zizi Banda <zizi.banda@firstbase.com>
2023-05-23 16:10:56 +0100

    add prettier properly

--- src/pages/btlCalculator.ts
+++ src/pages/btlCalculator.ts
@@ -1,4 +1,4 @@
-import { Page, Locator } from "@playwright/test";
+import { Page, Locator } from '@playwright/test';
 //PAGE OBJECT MODEL
 export class BTLCalculator {
   readonly page: Page;
@@ -15,15 +15,19 @@ export class BTLCalculator {
 
   constructor(page: Page) {
     this.page = page;
-    this.propertyValueInput = page.locator("#property-value-input");
-    this.loanAmountInput = page.locator('#loan-amount-input')
-    this.feeAmountInput = page.locator('#fee-amount-input')
-    this.monthlyRentalIncomeInput = page.locator('#monthly-rental-income-input')
-    this.applicationTypeDropdown = page.locator('#likeByLikeRemortgage-dropdown')
-    this.productTypeDropdown = page.locator('#productType-dropdown')
-    this.productRateInput = page.locator('#product-rate-input')
-    this.basicTaxRateDropdown = page.locator('#taxRate-dropdown')
-    this.loanToValueText = page.locator('#ltv-value')
-    this.outcomeResultText = page.locator('#cal-outcome-value') 
+    this.propertyValueInput = page.locator('#property-value-input');
+    this.loanAmountInput = page.locator('#loan-amount-input');
+    this.feeAmountInput = page.locator('#fee-amount-input');
+    this.monthlyRentalIncomeInput = page.locator(
+      '#monthly-rental-income-input',
+    );
+    this.applicationTypeDropdown = page.locator(
+      '#likeByLikeRemortgage-dropdown',
+    );
+    this.productTypeDropdown = page.locator('#productType-dropdown');
+    this.productRateInput = page.locator('#product-rate-input');
+    this.basicTaxRateDropdown = page.locator('#taxRate-dropdown');
+    this.loanToValueText = page.locator('#ltv-value');
+    this.outcomeResultText = page.locator('#cal-outcome-value');
   }
 }
